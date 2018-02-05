70a9d160538f6c6de57c1ffcb2f93c48a3bc53a3
wibbly13 <wesley.kirinya@gmail.com>
2018-02-05 22:49:41 +0300

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -8,6 +8,6 @@ VOLUMES=`pwd`
 docker run -d --name mongooseim-builder -h mongooseim-builder -v ${VOLUMES}/builds:/builds mongooseim/mongooseim-builder
 docker exec -i mongooseim-builder /build.sh
 cp builds/mongooseim* member/mongooseim.tar.gz
-docker build -f Dockerfile.member -t mongooseim:$1 .
-docker tag mongooseim mongooseim.azurecr.io/mongooseim:$1
+docker build -f Dockerfile.member -t mongooseim.azurecr.io/mongooseim:$1 .
+#docker tag mongooseim:$1 mongooseim.azurecr.io/mongooseim:$1
 docker images

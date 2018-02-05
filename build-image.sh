bfcf415795919599348e07343a6e9f9e2e8c992d
wibbly13 <wesley.kirinya@gmail.com>
2018-02-05 15:23:58 +0300

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -4,7 +4,7 @@ WORKING_DIR_MIM_DB='mongooseim-docker-build'
 mkdir ${WORKING_DIR_MIM_DB}
 cd ${WORKING_DIR_MIM_DB}
 git clone https://github.com/esl/mongooseim-docker.git .
-VOLUMES=`pwd`
+VOLUMES=${WORKING_DIR_MIM_DB}
 docker run -d --name mongooseim-builder -h mongooseim-builder -v ${VOLUMES}/builds:/builds mongooseim/mongooseim-builder
 docker exec -i mongooseim-builder /build.sh
 cp builds/mongooseim* member/mongooseim.tar.gz
